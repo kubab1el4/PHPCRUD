8acb7e3f596e28ba10fb5322bbd778566453922b
Jakub Bielanski <gg18051805@gmail.com>
2023-02-27 18:15:05 +0100

    Make some changes to database file

--- baza_samochody.sql
+++ baza_samochody.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Lut 2023, 23:08
+-- Czas generowania: 27 Lut 2023, 18:14
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -31,7 +31,7 @@ CREATE TABLE `auta` (
   `id` int(11) NOT NULL,
   `nazwa` varchar(30) NOT NULL,
   `max_speed` int(3) NOT NULL,
-  `silnik` varchar(20) NOT NULL,
+  `silnik` varchar(100) NOT NULL,
   `masa` decimal(5,0) NOT NULL,
   `cena` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -41,11 +41,11 @@ CREATE TABLE `auta` (
 --
 
 INSERT INTO `auta` (`id`, `nazwa`, `max_speed`, `silnik`, `masa`, `cena`) VALUES
-(63, 'Porsche_Panamera', 285, '3_6_litre_V6', '1814', '400000'),
+(63, 'Porsche_Panamera', 286, '3_6_litre_V6', '1815', '400000'),
 (64, 'Lexus_LS_350', 230, 'V6_Cylinder,_Hybrid,', '2186', '200000'),
 (65, 'Chevrolet_Camaro_ZL1', 318, '6_2_L_V-8', '1748', '300000'),
 (66, 'Dodge_Hellcat', 326, 'HEMI_V8', '2002', '474000'),
-(67, 'Volksvagen_ID__Buzz', 145, 'APP_310_permanent_ma', '3000', '200000');
+(138, 'Volkswagen_D_Buzz', 180, 'Elektryczny', '2000', '200000');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -55,7 +55,8 @@ INSERT INTO `auta` (`id`, `nazwa`, `max_speed`, `silnik`, `masa`, `cena`) VALUES
 -- Indeksy dla tabeli `auta`
 --
 ALTER TABLE `auta`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -65,7 +66,7 @@ ALTER TABLE `auta`
 -- AUTO_INCREMENT dla tabeli `auta`
 --
 ALTER TABLE `auta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=289;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
